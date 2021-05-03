96b230fc1c0d88d2a6476b1408e8673a5266ecb9
riskaagustin16 <riskaagustin@student.telkomuniversity.ac.id>
2021-05-04 03:49:46 +0700

    assessment2-6701194107

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Configuration for database connection
+ *
+ */
+
+$host       = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "pabw";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+?>
